87802ab3c74edc501f7b8adb80aef16b1536b657
salmanrf <salmanrf2@gmail.com>
2023-07-25 19:55:49 +0700

    feat: huggingface classifier module

--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@ from src.whatsapp_webhook.router import router as wa_webhook_router
 
 from src.config import config
 from src.databases.vector import vector_db
+from src.huggingface_module.hf import hf
 
 @asynccontextmanager
 async def lifespan(_application: FastAPI) -> AsyncGenerator:
+  hf.init()
   vector_db.init()
   
   yield
