5bfed29b78ce83976baa79634587b92bb1b5eb42
salman <salman@gradienttech.io>
2023-10-25 09:29:40 +0700

    feat: integrate with wingman matchmaking job

--- src/matchmaking/service.py
+++ src/matchmaking/service.py
@@ -1,12 +1,10 @@
 
-import requests
 from bson import ObjectId
 from fastapi import HTTPException, status
 
 import src.chat.service as chat_service
-import src.user.service as user_service
-import src.utils.func_utils as func_utils
 from src.config import config
+from src.wingman_api_client import WingmanAPIClient
 from src.exceptions import BaseException
 from src.wingman_api_client import WingmanAPIClient
 from src.config import config
@@ -27,21 +25,48 @@ async def handle_matchmaking_start(user_id: str, wingman_assistant_id: str) -> A
       "wingman_assistant_id": ObjectId(wingman_assistant_id)
     })
 
+    session = WingmanAPIClient.get_session()
+
+    chat_exports_job_url = f"{config.WINGMAN_BACKEND_API_URL}/jobs/wingman-chats-export"
+    init_matchmaking_job_url = f"{config.WINGMAN_BACKEND_API_URL}/jobs/matchmaking"
+
     # * Export chat history to discord channel
-    await func_utils.async_wrapper(
-      requests.post,
-      url=f"{config.WINGMAN_BACKEND_API_URL}/jobs/wingman-chats-export",
-      headers={
-        "client-id": config.SERVICE_USER_ID,
-        "client-secret": config.SERVICE_USER_SECRET
-      },
-      json= {
+    async with session.post(
+      chat_exports_job_url,
+      json={
         "wingman_chat_id": str(wingman_chat.id),
         "target": "discord"
       }
-    )
+    ) as export_res:
+      export_res_json = await export_res.json()
+    
+    if export_res.status != status.HTTP_200_OK:
+      raise HTTPException(
+        status_code=export_res.status,
+        detail=export_res_json["errors"] if "errors" in export_res_json else export_res_json
+      )
+    
+    # * Initiate matchmaking
+    async with session.post(
+      init_matchmaking_job_url,
+      json={
+        "user_id": user_id
+      }
+    ) as init_matchmaking_res:
+      init_matchmaking_res_json = await init_matchmaking_res.json()
 
+    if init_matchmaking_res.status != status.HTTP_200_OK:
+      raise HTTPException(
+        status_code=init_matchmaking_res.status,
+        detail=init_matchmaking_res_json["errors"] if "errors" in init_matchmaking_res_json else init_matchmaking_res_json
+      )
+    
     return True, None
+  except HTTPException as e:
+    print("HTTP ERROR at handle_matchmaking_start", e)
+
+    return False, e
+    
   except BaseException as e:
     print("ERROR at handle_matchmaking_start", e)
     return False, BaseException(detail={"handle_matchmaking_start": [e]})
